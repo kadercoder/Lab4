501bd6c18058fbf64933dfc614680b45d22e251b
“Abdulkader <akqayed@umich.edu>
2023-09-28 16:50:25 -0400

    added another printed line that says my age

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World, This is Abdulkader Qayed"+i);
+            System.out.println("I am 100 years old" +i);
         }
     }
 }
